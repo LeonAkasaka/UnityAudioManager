1d78d86f6c86d4d5c34af7ed2104fbac0d3be1a4
Leon Akasaka <leon@wisdomsoft.jp>
2015-02-02 01:03:46 +0900

    Added: Visual Studio solution and project.

---
+++ AudioManager/AudioManager/AudioInstance.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioInstance : MonoBehaviour
+{
+    /// <summary>
+    /// Gets a playing AudioClip.
+    /// </summary>
+    public AudioClip AudioClip { get; private set; }
+
+    /// <summary>
+    /// True if playing audio, otherwise false.
+    /// </summary>
+    public bool IsPlaying { get; private set; }
+
+    /// <summary>
+    /// Gets a audio category.
+    /// </summary>
+    public string Category { get; private set; }
+
+    private float _terminationSec = 0;
+    private bool _stopped = false;
+
+    /// <summary>
+    /// Play audio.
+    /// </summary>
+    /// <param name="clip"></param>
+    public void Play(AudioClip clip) { Play(clip, null); }
+
+    /// <summary>
+    /// Stop current playing audio.
+    /// </summary>
+    /// <param name="clip"></param>
+    /// <param name="category"></param>
+    public void Play(AudioClip clip, string category)
+    {
+        if (IsPlaying) { throw new System.InvalidOperationException(); }
+
+        AudioClip = clip;
+        Category = category;
+        StartCoroutine(IterateAudioPlaying());
+    }
+
+    /// <summary>
+    /// Stop current audio.
+    /// </summary>
+    public void Stop() { Stop(0); }
+
+    /// <summary>
+    /// Stop current playing audio.
+    /// </summary>
+    /// <param name="sec">Fade time.</param>
+    public void Stop(float sec)
+    {
+        _terminationSec = sec;
+        _stopped = true;
+    }
+
+    private IEnumerator IterateAudioPlaying()
+    {
+        IsPlaying = true;
+        gameObject.name = AudioClip.name;
+        audio.clip = AudioClip;
+        audio.Play();
+
+        while (!_stopped && audio.isPlaying)
+        {
+            yield return null;
+        }
+
+        yield return StartCoroutine(IterateTermination());
+
+        gameObject.name = "None";
+        audio.Stop();
+        IsPlaying = false;
+    }
+
+    private IEnumerator IterateTermination()
+    {
+        if (_terminationSec == 0)
+        {
+            yield break;
+        }
+
+        var t = 0F;
+        var v = audio.volume;
+        while (t < _terminationSec)
+        {
+            var p = 1 - _terminationSec / (_terminationSec - t);
+            audio.volume = v * p;
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
---
+++ AudioManager/AudioManager/AudioManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Audio Manager.
+/// </summary>
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _audioInstanceSource = null;
+    private GameObject Prefab { get { return _audioInstanceSource; } }
+
+    public IEnumerable<AudioInstance> MusicInstances { get { return _musicInstances; } }
+    private List<AudioInstance> _musicInstances = new List<AudioInstance>();
+
+    private void Start()
+    {
+        StartCoroutine(IterateAudioManager());
+    }
+
+    private IEnumerator IterateAudioManager()
+    {
+        while (true)
+        {
+            var garbage = MusicInstances.Where(x => !x.IsPlaying).ToArray();
+            foreach (var i in garbage)
+            {
+                _musicInstances.Remove(i);
+                GameObject.Destroy(i.gameObject);
+            }
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    /// <summary>
+    /// Play AudioClip as a BGM.
+    /// </summary>
+    /// <param name="bgm">AudioClip.</param>
+    /// <returns>AudioInstance.</returns>
+    public AudioInstance PlayBgw(AudioClip bgm)
+    {
+        const string BGM = "BGM";
+
+        var current = _musicInstances.FirstOrDefault(x => x.AudioClip == bgm && x.Category == BGM);
+        if (current != null)
+        {
+            return current;
+        }
+
+        var ai = Play(bgm, BGM);
+        _musicInstances.Add(ai);
+        return ai;
+    }
+
+    /// <summary>
+    /// Play AudioClip as a sound effect.
+    /// </summary>
+    /// <param name="se">AudioClip.</param>
+    /// <returns>AudioInstance.</returns>
+    public AudioInstance PlayEffect(AudioClip se)
+    {
+        return Play(se, "SoundEffec");
+    }
+
+    private AudioInstance Play(AudioClip audioClip, string category)
+    {
+        var ai = GetAudioInstance();
+        ai.Play(audioClip, category);
+        return ai;
+    }
+
+    private AudioInstance GetAudioInstance()
+    {
+        var obj = GameObject.Instantiate(Prefab) as GameObject;
+        obj.transform.parent = transform;
+        var ai = obj.GetComponent<AudioInstance>();
+        return ai;
+    }
+}
